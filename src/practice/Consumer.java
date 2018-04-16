a7fd1b9c43d4c50a3870116a130660735f78c60f
spandey280191 <35771851+spandey280191@users.noreply.github.com>
2018-04-16 16:12:27 +0200

    assignment

---
+++ src/practice/Consumer.java
@@ -0,0 +1,31 @@
+package com.assignment.model;
+
+public class Consumer implements Runnable {
+
+	private Player p;
+		
+	Consumer(Player p){
+		this.p=p;
+	}
+	
+	public void run() {
+		
+		receiveMessage(p);
+	}
+
+	public void receiveMessage(Player p){
+		try {
+			String message;
+			while(true){
+			message = p.getQueue().take();
+	        System.out.println(p.getPalyer2().getName()+":"+message);
+	        p.getPalyer2().getPlayerHelper().sendMessage(p.getPalyer2(), message);
+			
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+			
+	}
+	
+}
